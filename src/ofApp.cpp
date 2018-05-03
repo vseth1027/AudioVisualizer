10c0bd50bfb55a3ecdba9b9e3b177eebf5ef5a3b
Vidushi Seth <vidushi.seth@gmail.com>
2018-05-02 21:04:31 -0500

    Added comments for clarity and edited formatting

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -28,55 +28,28 @@ void ofApp::update(){
     
     ofSetWindowTitle(ofToString(ofGetFrameRate()));
     
-    //-:Get buffer from sound player:
     soundBuffer = player.getCurrentSoundBuffer(bufferSize);
     
-    //-:ANALYZE SOUNDBUFFER:
     audioAnalyzer.analyze(soundBuffer);
     
-    //-:get Values:
+    //Get the values from the audio analyzer
     rms     = audioAnalyzer.getValue(RMS, 0, smoothing);
     power   = audioAnalyzer.getValue(POWER, 0, smoothing);
     pitchFreq = audioAnalyzer.getValue(PITCH_FREQ, 0, smoothing);
     pitchConf = audioAnalyzer.getValue(PITCH_CONFIDENCE, 0, smoothing);
     pitchSalience  = audioAnalyzer.getValue(PITCH_SALIENCE, 0, smoothing);
-    inharmonicity   = audioAnalyzer.getValue(INHARMONICITY, 0, smoothing);
-    hfc = audioAnalyzer.getValue(HFC, 0, smoothing);
-    specComp = audioAnalyzer.getValue(SPECTRAL_COMPLEXITY, 0, smoothing);
     centroid = audioAnalyzer.getValue(CENTROID, 0, smoothing);
-    rollOff = audioAnalyzer.getValue(ROLL_OFF, 0, smoothing);
-    oddToEven = audioAnalyzer.getValue(ODD_TO_EVEN, 0, smoothing);
-    strongPeak = audioAnalyzer.getValue(STRONG_PEAK, 0, smoothing);
-    strongDecay = audioAnalyzer.getValue(STRONG_DECAY, 0, smoothing);
     
-    
-    //Normalized values for graphic meters:
+    //Normalized values (between 0 and 1)
     pitchFreqNorm   = audioAnalyzer.getValue(PITCH_FREQ, 0, smoothing, TRUE);
-    hfcNorm     = audioAnalyzer.getValue(HFC, 0, smoothing, TRUE);
-    specCompNorm = audioAnalyzer.getValue(SPECTRAL_COMPLEXITY, 0, smoothing, TRUE);
     centroidNorm = audioAnalyzer.getValue(CENTROID, 0, smoothing, TRUE);
-    rollOffNorm  = audioAnalyzer.getValue(ROLL_OFF, 0, smoothing, TRUE);
-    oddToEvenNorm   = audioAnalyzer.getValue(ODD_TO_EVEN, 0, smoothing, TRUE);
-    strongPeakNorm  = audioAnalyzer.getValue(STRONG_PEAK, 0, smoothing, TRUE);
-    strongDecayNorm = audioAnalyzer.getValue(STRONG_DECAY, 0, smoothing, TRUE);
-    
     dissonance = audioAnalyzer.getValue(DISSONANCE, 0, smoothing);
-    
-    spectrum = audioAnalyzer.getValues(SPECTRUM, 0, smoothing);
-    melBands = audioAnalyzer.getValues(MEL_BANDS, 0, smoothing);
-    mfcc = audioAnalyzer.getValues(MFCC, 0, smoothing);
-    hpcp = audioAnalyzer.getValues(HPCP, 0, smoothing);
-    
-    tristimulus = audioAnalyzer.getValues(TRISTIMULUS, 0, smoothing);
-    
-    isOnset = audioAnalyzer.getOnsetValue(0);
   
    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    
     
     ofTranslate(350, 0);
     int max_width = 150;
@@ -89,59 +62,55 @@ void ofApp::draw(){
     
     float value, valueNorm;
     
-    ofSetColor(255);
+    //draw rms
     value = rms;
     ofSetColor(ofColor::cyan);
     ofDrawCircle(xpos, ofApp::kyposition, value * max_width);
     
+    //draw power
     incrementXandY(xpos, deltax, ypos, deltay);
-    ofSetColor(255);
     value = power;
     ofSetColor(ofColor::paleVioletRed);
     ofDrawCircle(xpos, ofApp::kyposition, value * max_width);
     
-    
+    //draw pitch frequency
     incrementXandY(xpos, deltax, ypos, deltay);
-    ofSetColor(255);
     value = pitchFreq;
     valueNorm = pitchFreqNorm;
     ofSetColor(ofColor::purple);
     ofDrawCircle(xpos, valueNorm * ofApp::kheight, half_max_width);
 
+    //draw pitch confidence
     incrementXandY(xpos, deltax, ypos, deltay);
-    ofSetColor(255);
     value = pitchConf;
     ofSetColor(ofColor::orange);
     ofDrawCircle(xpos, value * ofApp::kheight, half_max_width);
     
+    //draw centroid
+    //centroid corresponds to the "brightness" of the sound
+    //tends to be the opposite image of rms/power
     incrementXandY(xpos, deltax, ypos, deltay);
-    ofSetColor(255);
     value = centroid;
     valueNorm = centroidNorm;
     ofSetColor(ofColor::blanchedAlmond);
     ofDrawCircle(xpos, ofApp::kyposition, valueNorm * max_width);
-    //centroid corresponds to the "brightness" of the sound
-    //tends to be the opposite image of rms/power
-    
 
+    //draw dissonance
     incrementXandY(xpos, deltax, ypos, deltay);
-    ofSetColor(255);
     value = dissonance;
     ofSetColor(ofColor::lime);
     ofDrawCircle(xpos, ypos, value * max_width);
     
-  
+    //draw pitch salience
     incrementXandY(xpos, deltax, ypos, deltay);
-    ofSetColor(255);
     value = pitchSalience;
     ofSetColor(ofColor::cornflowerBlue);
     ofDrawCircle(xpos, value * ofApp::kheight, one_third_max_width);
     
-    //-Gui & info:
+    //draw GUI and other text
     
     gui.draw();
-    ofSetColor(255);
-    ofSetColor(ofColor::hotPink);
+    ofSetColor(ofColor::mistyRose);
     ofDrawBitmapString("Keys 1-9: Play other audio tracks", 0, 10);
     
 }
